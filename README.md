8d9330194f81e44610b5b920e605f6b32356bd7f
Julia Kieserman <jbk67@georgetown.edu>
2017-06-02 21:32:03 -0500

    completed project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ud036_StarterCode
-Source code for a Movie Trailer website.
+# Movies Website
+
+Website to view and preview trailers of my favorite movies
+
+## Running
+
+On the command line, navigate to the directory containing all files. Excute python file 'entertainment_center.py'
+
+### Customization
+
+To customize which movies are displayed, enter the main function of 'entertainment_center.py' and add the following line of code:
+create_movie_object(YOUR_MOVIE_TITLE)
---
+++ entertainment_center.py
@@ -0,0 +1,37 @@
+import media
+import fresh_tomatoes
+import requests
+
+data_string = "https://api.themoviedb.org/3/search/movie?api_key=0bf21ff60196bf22bce9136c1db1da7c&query="
+movies = []
+
+def create_movie_object(movie):
+	search_params = ''
+	#create search params to feed to api call
+	for word in movie.split():
+		search_params += word+'+'
+	search_movie = data_string + search_params
+	
+	# call movie api for requested movie
+	r = requests.get(search_movie)
+	# create dictionary of results
+	resp = r.json()['results'][0]
+
+	#create poster url
+	poster_link = 'http://image.tmdb.org/t/p/w185/' + resp['poster_path']
+
+	#use movie ID to request the trailer and construct youtube url
+	trailer = requests.get("http://api.themoviedb.org/3/movie/" + str(resp['id']) + "/videos?api_key=0bf21ff60196bf22bce9136c1db1da7c")
+	youtube_link = "https://www.youtube.com/watch?v=" + trailer.json()['results'][0]['key']
+
+	#add new movie object to list of movies
+	movies.append(media.Movie(resp['original_title'], resp['overview'], poster_link, youtube_link))
+
+if __name__ == '__main__':
+	create_movie_object("School of Rock")
+	create_movie_object("Toy Story")
+	create_movie_object("Ratatouille")
+	create_movie_object("Midnight In Paris")
+	create_movie_object("Hunger Games")
+
+	fresh_tomatoes.open_movies_page(movies)
